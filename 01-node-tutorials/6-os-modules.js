d46cf4b92b55c5c0981cbff290ce075f26037680
AqibNiazi <aqibjaved5201@gmail.com>
2025-11-14 12:08:45 +0500

    OS modules info added

--- 01-node-tutorials/6-os-modules.js
+++ 01-node-tutorials/6-os-modules.js
@@ -1,20 +1,19 @@
 //working with OS module
 const os = require("os");
 
-// Get user info    
+// Get user info
 const user = os.userInfo();
 console.log(user);
 
 //Get system uptime in seconds
 console.log(`The system uptime is ${os.uptime()} seconds`);
 
-
 //Getting current OS info
 const currentOS = {
-    name: os.type(),
-    release: os.release(),
-    totalMem: os.totalmem(),
-    freeMem: os.freemem(),
+  name: os.type(),
+  release: os.release(),
+  totalMem: os.totalmem(),
+  freeMem: os.freemem(),
 };
 
 console.log(currentOS);
