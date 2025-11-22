8a8872e2f1b2318e859c4ad6fe2279c2ad49c013
AqibNiazi <aqibjaved5201@gmail.com>
2025-11-22 17:06:28 +0500

    http and nodemon

--- 01-node-tutorials/app.js
+++ 01-node-tutorials/app.js
@@ -1,14 +1,1 @@
-// Working with file paths
-
-const path = require("path");
-
-console.log(path.sep);
-
-const filePath = path.join("/content", "test.txt");
-console.log(filePath);
-
-const base = path.basename(filePath);
-console.log(base);
-
-const absolute = path.resolve(__dirname, "content", "test.txt");
-console.log(absolute);
+console.log("Hello World");
