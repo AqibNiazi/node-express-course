5e21987efba76a1aa4249afac94c14b84101c4a4
AqibNiazi <aqibjaved5201@gmail.com>
2025-11-27 20:21:50 +0500

    Express Basics covered

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,19 @@
-console.log('Express Tutorial')
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).send("Home Page");
+});
+
+app.get("/about", (req, res) => {
+  res.status(200).send("About page");
+});
+
+app.all("*", (req, res) => {
+  res.status(404).send("<h1>Resource not found</h1>");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
+module.exports = app;
