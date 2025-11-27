5f9dca69cf64fa92eb8e2e9f6df236f3c39d1f3f
AqibNiazi <aqibjaved5201@gmail.com>
2025-11-27 21:48:59 +0500

    setting up static files and middleware

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,19 +1,19 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+
+// setup static and middleware
+app.use(express.static("./public"));
 
 app.get("/", (req, res) => {
-  res.status(200).send("Home Page");
-});
-
-app.get("/about", (req, res) => {
-  res.status(200).send("About page");
+  res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
 });
 
 app.all("*", (req, res) => {
   res.status(404).send("<h1>Resource not found</h1>");
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
 });
 module.exports = app;
